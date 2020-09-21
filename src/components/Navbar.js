1327e1f9eef3a1aae38dd947d929513916b733fb
Cedric Johnson <cedric@unaffiliatedstudios.com>
2020-09-21 03:35:22 -0500

    Add new component files; styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react'
 import NavItem from './NavItem'
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'NavItemActive': ''
+        }
+    }
+
+    activeitem = (x) => {
+        if(this.state.NavItemActive.length > 0) {
+            document.getElementById(this.state.NavItemActive).classList.remove('active')
+        }
+        this.setState({'NavItemId':x}, () => {
+            document.getElementById(this.state.NavItemActive).classList.add('active')
+        })
+    }
+
     render() {
         return (
             <nav>
                 <ul>
-                    <NavItem item="Home" tolink="/"></NavItem>
-                    <NavItem item="About" tolink="/about"></NavItem>
-                    <NavItem item="Education" tolink="/education"></NavItem>
-                    <NavItem item="Skills" tolink="/skills"></NavItem>
-                    <NavItem item="Contact" tolink="/contact"></NavItem>
+                    <NavItem item="Home" activec={this.activeitem} tolink="/"></NavItem>
+                    <NavItem item="About" activec={this.activeitem} tolink="/about"></NavItem>
+                    <NavItem item="Education" activec={this.activeitem} tolink="/education"></NavItem>
+                    <NavItem item="Skills" activec={this.activeitem} tolink="/skills"></NavItem>
+                    <NavItem item="Contact" activec={this.activeitem} tolink="/contact"></NavItem>
                 </ul>
             </nav>
         )
