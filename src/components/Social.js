a1aa57615dc36f37a82545cea1acbd97fb46f147
Cedric Johnson <cedric@unaffiliatedstudios.com>
2020-09-22 20:19:33 -0500

    Change link to render Instagram logo

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,7 +7,7 @@ class Social extends Component {
             <div className="social">
                 <SocialIcon url="https://codepen.io/fireside68" target="_blank" rel="noopener noreferrer" />
                 <SocialIcon url="https://github.com/fireside68" target="_blank" rel="noopener noreferrer" />
-                <SocialIcon url="https://Instagram.com/fireside68" target="_blank" rel="noopener noreferrer" />
+                <SocialIcon url="https://instagram.com/fireside68" target="_blank" rel="noopener noreferrer" />
                 <SocialIcon url="https://www.facebook.com/cedric.johnson.391082" target="_blank" rel="noopener noreferrer" />
                 <SocialIcon url="https://www.linkedin.com/in/cedric-johnson-88464380/" target="_blank" rel="noopener noreferrer" />
             </div>
